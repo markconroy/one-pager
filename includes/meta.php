8e25bdca87a6628d2b57290c9d2055b0993e2c04
Mark Conroy <info@adesignforlife.net>
2015-02-23 16:35:45 +0000

    changing stylesheets folder to css folder

--- includes/meta.php
+++ includes/meta.php
@@ -3,7 +3,7 @@
   <title>A One Page Website, built by Mark Conroy - mark.ie</title>
   <meta name="HandheldFriendly" content="true">
   <meta name="viewport" content="width=device-width, initial-scale=1,maximum-scale=1,user-scalable=no">
-  <link href="/stylesheets/screen.css" media="screen, projection" rel="stylesheet" type="text/css" />
+  <link href="/css/screen.css" media="screen, projection" rel="stylesheet" type="text/css" />
   <link href="/stylesheets/print.css" media="print" rel="stylesheet" type="text/css" />
   <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
    <script src="/script.js"></script>
